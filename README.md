a810b5750d38ee07b348e0c656cfc418dd928a61
vinayakhalgekar97 <vh.developer200817@gmail.com>
2025-05-02 13:26:03 +0530

    weather application completed.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # weather-application
 Weather Application
+
+Prerequisite Knowledge:
+1. Python Programming Language
+2. JSON
+3. requests python library. documentation link: https://requests.readthedocs.io/en/latest/
--- main.py
+++ main.py
@@ -4,10 +4,12 @@ import os
 
 load_dotenv()
 
-city = input("Enter city name: ") 
+def get_current_weather_forecast():
+    city = input("Enter City Name: ") 
+    request_url = f"https://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=metric"
+    response = requests.get(request_url).json()
+    print("******** CURRENT WEATHER DATA *********")
+    return f"City: {response['name']}\nCurrent Temperature: {response['main']['temp']} Degree Celsius. \nHumidity: {response['main']['humidity']}% \nDescription: {response['weather'][0]['description']}"
 
-request_url = f"https://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=metric"
-
-response = requests.get(request_url)
-
-print(response.json())
+if __name__ == "__main__":
+    print(get_current_weather_forecast())
